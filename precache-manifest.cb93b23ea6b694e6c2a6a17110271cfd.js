443a35e4b7021c2d420117323764ea60044c89d2
Dasha <smidariaa@gmail.com>
2020-09-17 13:14:39 +0300

    Updates

---
+++ precache-manifest.cb93b23ea6b694e6c2a6a17110271cfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f4275f41afbfc75f3ad01d3693c74d9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "33ad1a8b345661649fdc",
+    "url": "/portfolio/static/css/main.4f136b45.chunk.css"
+  },
+  {
+    "revision": "8343cd71869a07d380d0",
+    "url": "/portfolio/static/js/2.b4b604e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.b4b604e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33ad1a8b345661649fdc",
+    "url": "/portfolio/static/js/main.24a11c25.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
